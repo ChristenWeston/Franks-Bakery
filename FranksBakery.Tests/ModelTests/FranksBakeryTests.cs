4e63e86f47cac16b1c70641496f7ad08b40513e6
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 13:46:57 -0800

    BreadPrice test passing

--- FranksBakery.Tests/ModelTests/FranksBakeryTests.cs
+++ FranksBakery.Tests/ModelTests/FranksBakeryTests.cs
@@ -15,5 +15,14 @@ namespace FranksBakery.Tests
       BreadStuff newBread = new BreadStuff("2");
       Assert.AreEqual(typeof(BreadStuff), newBread.GetType());
     }
+
+    [TestMethod]
+    public void FranksBakery_BreadPriceReturnsCorrectPriceFor5Loaves()
+    {
+      BreadStuff new5LoafBread = new BreadStuff("5");
+      double result = 20.00;
+      Assert.AreEqual(new5LoafBread.breadCost, result);
+    }
+
   }
 }
