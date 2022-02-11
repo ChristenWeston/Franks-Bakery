127623a33accb064b35bfe2482475d6b36dacbb8
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 14:58:54 -0800

    Initial pastry class created

---
+++ FranksBakery/Models/Pastry.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System;
+
+namespace Pastry
+{
+  public class PastryStuff
+  {
+    public int pastryQuantity {get; set;}
+    public double pastryCost {get; set;}
+
+    
+  }
+}
