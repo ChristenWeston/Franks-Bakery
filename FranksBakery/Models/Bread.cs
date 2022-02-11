0d88ffe808a599ce7a60371294e4a761e51d9a6a
Christen Weston <paleeserecycle@gmail.com>
2022-02-10 20:35:33 -0800

    Initial bread constructor created

--- FranksBakery/Models/Bread.cs
+++ FranksBakery/Models/Bread.cs
@@ -5,6 +5,19 @@ namespace Bread
 {
   public class BreadStuff
   {
-    
+    public int breadQuantity {get; set;}
+    public int breadCost {get; set;}
+
+    //Constructor
+    public Breadstuff(string quantity)
+    {
+      int quantityToInt;
+      Int32.TryParse(quantity, out quantityToInt);
+      //breadCost will equal a function that calculates the cost
+    }
   }
 }
+
+//Buy 2, get 1 free (every 3rd loaf of bread is free.). 
+//A single loaf costs $5, 
+//two loaves costs $10, and three loaves cost $10.
