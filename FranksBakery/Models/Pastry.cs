d6bb243af3d731d31a8d517987ba9ec1ed1328c5
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 15:03:27 -0800

    Pastry constructor created

--- FranksBakery/Models/Pastry.cs
+++ FranksBakery/Models/Pastry.cs
@@ -8,6 +8,17 @@ namespace Pastry
     public int pastryQuantity {get; set;}
     public double pastryCost {get; set;}
 
-    
+    //Constructor
+    public PastryStuff(string quantity)
+    {
+      int quantityToInt;
+      Int32.TryParse(quantity, out quantityToInt);
+      pastryQuantity = quantityToInt;
+      //pastryCost = //PastryCost function
+    }
   }
 }
+
+//Buy 1 for $2 or 3 for $5. 
+//Four pastries costs $7, five pastries costs $9,
+// and six pastries costs $10.
