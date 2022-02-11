ae17ce33f8e54ea20a70106981913c637451c271
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 15:44:14 -0800

    Pastry cost function written

--- FranksBakery/Models/Pastry.cs
+++ FranksBakery/Models/Pastry.cs
@@ -14,7 +14,14 @@ namespace Pastry
       int quantityToInt;
       Int32.TryParse(quantity, out quantityToInt);
       pastryQuantity = quantityToInt;
-      //pastryCost = //PastryCost function
+      pastryCost = PastryCost(pastryQuantity);
+    }
+
+    public double PastryCost(int pastryQuantity)
+    {
+      pastryCost = ((pastryQuantity / 3) * 1.00) + ((pastryQuantity - (pastryQuantity / 3)) * 2.00);
+      Console.WriteLine("Pastry cost is : " + pastryCost);
+      return pastryCost;
     }
   }
 }
