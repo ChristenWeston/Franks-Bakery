4c405118642d6261cb9075e83c699bad44cea0f8
Christen Weston <paleeserecycle@gmail.com>
2022-02-10 20:42:24 -0800

    Initial program now asks for quantity of bread and calls bread constructor

--- FranksBakery/Models/Program.cs
+++ FranksBakery/Models/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Bread;
 
 namespace MainProgram
 {
@@ -8,6 +9,13 @@ namespace MainProgram
     public static void Main()
     {
       Console.WriteLine("Welcome to Frank's Bakery!");
+      //Here is the price for bread and pastry
+      Console.Write("How many loaves of bread would you like?: ");
+      string loavesOfBread = Console.ReadLine();
+
+      BreadStuff breadOrder = new BreadStuff(loavesOfBread);
+      Console.WriteLine(breadOrder.breadQuantity);
+
     }
   }
 }
