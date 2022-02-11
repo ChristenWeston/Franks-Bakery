672f30869617ede5ca43bee2b3120c0150e5c3ba
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 15:04:32 -0800

    Updated program to include pastries

--- FranksBakery/Models/Program.cs
+++ FranksBakery/Models/Program.cs
@@ -8,10 +8,12 @@ namespace MainProgram
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Frank's Bakery! Bread is $5.00 a loaf and pastry is $3.50 each");
+      Console.WriteLine("Welcome to Frank's Bakery! Bread is $5.00 a loaf and pastry is $2.00 each or 3 for $5.00");
       //Here is the price for bread and pastry
       Console.Write("How many loaves of bread would you like?: ");
       string loavesOfBread = Console.ReadLine();
+      Console.Write("How many pastries would you like?: ");
+      string numberOfPastries = Console.ReadLine();
 
       BreadStuff breadOrder = new BreadStuff(loavesOfBread);
       Console.WriteLine(breadOrder.breadQuantity);
