e40944622eba770da5bd87631dbc0056ab39e19f
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 15:55:31 -0800

    Cleaned up Console.Writelines and notes

--- FranksBakery/Models/Pastry.cs
+++ FranksBakery/Models/Pastry.cs
@@ -8,7 +8,6 @@ namespace Pastry
     public int pastryQuantity {get; set;}
     public double pastryCost {get; set;}
 
-    //Constructor
     public PastryStuff(string quantity)
     {
       int quantityToInt;
@@ -20,12 +19,7 @@ namespace Pastry
     public double PastryCost(int pastryQuantity)
     {
       pastryCost = ((pastryQuantity / 3) * 1.00) + ((pastryQuantity - (pastryQuantity / 3)) * 2.00);
-      Console.WriteLine("Pastry cost is : " + pastryCost);
       return pastryCost;
     }
   }
 }
-
-//Buy 1 for $2 or 3 for $5. 
-//Four pastries costs $7, five pastries costs $9,
-// and six pastries costs $10.
