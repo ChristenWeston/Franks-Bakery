d8632a31561db58dd4fd31ce30bdc29f3f9ed013
Christen Weston <paleeserecycle@gmail.com>
2022-02-11 15:31:01 -0800

    Cleaned up main and creating new pastry order

--- FranksBakery/Models/Program.cs
+++ FranksBakery/Models/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Bread;
+using Pastry;
 
 namespace MainProgram
 {
@@ -9,7 +10,6 @@ namespace MainProgram
     public static void Main()
     {
       Console.WriteLine("Welcome to Frank's Bakery! Bread is $5.00 a loaf and pastry is $2.00 each or 3 for $5.00");
-      //Here is the price for bread and pastry
       Console.Write("How many loaves of bread would you like?: ");
       string loavesOfBread = Console.ReadLine();
       Console.Write("How many pastries would you like?: ");
