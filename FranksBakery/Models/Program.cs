4a07aa7e74728d966bd2a88ad35bf13b9dae66d2
Christen Weston <paleeserecycle@gmail.com>
2022-02-10 19:08:27 -0800

    Test folder and models folder created

---
+++ FranksBakery/Models/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace MainProgram
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Frank's Bakery!");
+    }
+  }
+}
