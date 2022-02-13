f0d570b2d712fcbbdf9b3e610d4ec201ab38fd5d
Christen Weston <paleeserecycle@gmail.com>
2022-02-12 19:07:56 -0800

    Cleaned up Console.WriteLines in Bread.cs

--- FranksBakery/Models/Bread.cs
+++ FranksBakery/Models/Bread.cs
@@ -8,7 +8,6 @@ namespace Bread
     public int breadQuantity {get; set;}
     public double breadCost {get; set;}
 
-    //Constructor
     public BreadStuff(string quantity)
     {
       int quantityToInt;
@@ -22,12 +21,10 @@ namespace Bread
       if (breadQuantity <3)
       {
         breadCost = breadQuantity * 5.00;
-        Console.WriteLine("breadcost is: " + breadCost);
       }
       else if (breadQuantity >= 3)
       {
         breadCost = (breadQuantity - (breadQuantity / 3)) * 5.00;
-        Console.WriteLine("breadcost is: " + breadCost);
       }
       return breadCost;
     }
